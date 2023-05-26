afdc342f77e6cc6d0469828c12243284a8088c88
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-05-25 20:30:07 -0700

    Create AddTwoIntegers.cpp

---
+++ Add Two Integers/AddTwoIntegers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        //variable declaration
+        int result;
+
+        //check contstraint then proceed with operation
+        if(num1 >= -100 && num2 <= 100){
+            return num1 + num2;
+            }
+        return result;
+    }
+};
