3b84dddecf0e47912f9ff105df1aedeea2eb9405
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-05-31 18:56:14 -0700

    Update SubtractTheProductAndSumOfDigitsOfAnInteger.cpp

--- Subtract The Product And Sum Of Digits Of An Integer/SubtractTheProductAndSumOfDigitsOfAnInteger.cpp
+++ Subtract The Product And Sum Of Digits Of An Integer/SubtractTheProductAndSumOfDigitsOfAnInteger.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-       //code here
+        //variable declaration
+        long product = 1;
+        int sum = 0;
+        int ans;
+        int element;
+
+        vector<int> temp; //vector declaration
+
+        //fill vector indivudally with each element
+        while(n > 0){
+            element = n % 10;
+            temp.push_back(element);
+            n /= 10;
+        }
+        
+        //solve for the product and sum
+        for(int i = 0; i < temp.size(); i++){
+            product *= temp[i];
+            sum += temp[i];
+        }
+        
+        //subtract product and sum and return answer
+        ans = product - sum;
+        return ans;
     }
 };
