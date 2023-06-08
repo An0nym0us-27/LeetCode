1fd6861f7e8d0566a37964bdd5575fdc2e43b403
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-06-07 22:18:32 -0700

    Update XOROperationInAnArray.cpp

--- XOR Operation In An Array/XOROperationInAnArray.cpp
+++ XOR Operation In An Array/XOROperationInAnArray.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        
+        int temp = start;
+
+        int* nums= new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = (start + 2 * i);
+            temp ^= nums[i];
+        }
+        delete[] nums;
+
+        return temp;
     }
 };
