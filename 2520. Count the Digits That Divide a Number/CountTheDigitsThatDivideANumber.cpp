e23b8cb0be5aa9a4c51e6663512c88f55c9756d4
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-06-11 15:59:13 -0700

    Update CountTheDigitsThatDivideANumber.cpp

--- 2520. Count the Digits That Divide a Number/CountTheDigitsThatDivideANumber.cpp
+++ 2520. Count the Digits That Divide a Number/CountTheDigitsThatDivideANumber.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int countDigits(int num) {
+        int counter = 0;
+        int element;
+        int val;
+        int nums = num;
+
+        vector<int> myvec; //declare vector
+
+        //individually store each integer of num in myvec
+        while(num > 0){
+            element = num % 10;
+            myvec.push_back(element);
+            num /= 10;
+        }
+
+        //count how many elements can divide nums
+        for(int i = 0; i < myvec.size(); i++){
+            val = myvec[i];
+            if(nums % val == 0){
+                counter++;
+            }
+        }
         
+        return counter;
     }
 };
