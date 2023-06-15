2dd525d3f99ce9dc1e62b6e841ee2cbd2d04f0dd
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-06-14 19:13:09 -0700

    Update DifferenceBetweenElementSumAndDigitSumOfAnArray.cpp

--- 2535. Difference Between Element Sum And Digit Sum Of An Array/DifferenceBetweenElementSumAndDigitSumOfAnArray.cpp
+++ 2535. Difference Between Element Sum And Digit Sum Of An Array/DifferenceBetweenElementSumAndDigitSumOfAnArray.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     int differenceOfSum(vector<int>& nums) {
+        int x = 0; //element sum
+        int y = 0; //digit sum
+        int eh; //element holder
         
+        int tempInt;
+        vector<int> temp;
+
+
+        for(int i = 0; i < nums.size(); i++){
+            x += nums[i]; //finds element sum
+            eh = nums[i];
+
+            //stores each element into temp vector
+            while(eh > 0){
+                tempInt = eh % 10;
+                temp.push_back(tempInt);
+                eh /= 10;
+            }
+        }
+        for(int i = 0; i < temp.size(); i++){
+            y += temp[i]; //finds digit sum
+        }
+
+        int difference = abs(x - y); //take absolute val of x - y
+
+        return difference;
     }
 };
