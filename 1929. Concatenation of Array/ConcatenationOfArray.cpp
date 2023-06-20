a136015b11fc613a949eb994874996f46d0646c4
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-06-19 20:43:52 -0700

    Update ConcatenationOfArray.cpp

--- 1929. Concatenation of Array/ConcatenationOfArray.cpp
+++ 1929. Concatenation of Array/ConcatenationOfArray.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        
+        int n = nums.size(); //nums size
+        int size = 2 * n; //var for scalability
+        vector<int> ans(size); //vector declaration
+
+        for(int i = 0; i < n; i++){ //fill indices 0-2
+            ans[i] = nums[i];
+        }
+        for(int i = n; i < size; i++){ //fill indices 3-5
+            ans[i] = nums[i-n];
+        }
+
+        return ans; //return concatenated vector
     }
 };
