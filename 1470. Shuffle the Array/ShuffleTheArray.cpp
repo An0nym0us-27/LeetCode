6827b4f94ba3a588552209ba7733269aa5ff6d8a
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-06-21 23:22:10 -0700

    Update ShuffleTheArray.cpp

--- 1470. Shuffle the Array/ShuffleTheArray.cpp
+++ 1470. Shuffle the Array/ShuffleTheArray.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        
+        int size = nums.length;
+        int middle = (size / 2)
+
+        if(size % 2 == 0){
+            for(int i = 1; i <= middle; i++){
+                for(int j = middle; j < size; j++){
+                    int next = nums[i+1];
+                    
+                    temp = nums[i];
+                    nums[i] = nums[j];
+
+
+                }
+            }
+        }
     }
 };
