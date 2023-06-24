dc2338318691979022614b7f8f29b5cea2f61565
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-06-23 23:02:53 -0700

    Update FinalValueOfVariableAfterPerformingOperations.cpp

--- 2011. Final Value of Variable After Performing Operations/FinalValueOfVariableAfterPerformingOperations.cpp
+++ 2011. Final Value of Variable After Performing Operations/FinalValueOfVariableAfterPerformingOperations.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        
+        int X = 0; //counter
+        int l = operations.size(); //var for code scalability
+
+        for(int i = 0; i < l; i++){ //increment if string is ++X or X++
+            if((operations[i] == "X++") ||  (operations[i] == "++X")){
+                X++;
+            }
+            else{ //decrement if string is --X or X--
+                X--;
+            }
+        }
+
+        return X; //return counter
     }
 };
