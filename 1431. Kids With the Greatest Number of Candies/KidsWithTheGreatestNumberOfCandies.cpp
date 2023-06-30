38af0aea79a565f4b6ebbd8543ae9fb24a29c173
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-06-29 17:36:30 -0700

    Update KidsWithTheGreatestNumberOfCandies.cpp

--- 1431. Kids With the Greatest Number of Candies/KidsWithTheGreatestNumberOfCandies.cpp
+++ 1431. Kids With the Greatest Number of Candies/KidsWithTheGreatestNumberOfCandies.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        
+        int most = 0; //value of kid with most candies
+        int cpe = 0; //cpe = candies plus extra
+
+        vector<bool> result; //output vector instantiation
+
+        //determine most candies value
+        for(int i = 0; i < candies.size(); i++){
+            if(candies[i] > most){ //if current index is greater than previous most value, then update val
+                most = candies[i];
+            }
+        }
+        //compare cpe of every index with most candies
+        for(int i = 0; i < candies.size(); i++){
+            cpe = candies[i] + extraCandies;
+            if(cpe >= most){ //if cpe is greater than equal to most, then push back true
+                result.push_back(true);
+            }
+            else{ //else push back false
+                result.push_back(false);
+            }
+        }
+    return result; //returns result
     }
 };
