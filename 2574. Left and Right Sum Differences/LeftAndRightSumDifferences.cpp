203fc8692f4957074f852ae818826fc780c708df
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-07-01 19:03:55 -0700

    Update LeftAndRightSumDifferences.cpp

--- 2574. Left and Right Sum Differences/LeftAndRightSumDifferences.cpp
+++ 2574. Left and Right Sum Differences/LeftAndRightSumDifferences.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
-        
+        vector<int> answer;
+
+        for(int i = 0; i < nums.size(); i++){
+            if((leftSum[i] = nums[0]) || (rightSum[i] = nums.size())){ //should -1 size of nums?
+                leftSum[i] == 0;
+                rightSum[i] == 0;
+            }
+            else{
+                
+            }
+        }
     }
 };
