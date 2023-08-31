5da293fc1120ad3007a882b545e49c80334f216b
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2023-08-30 21:28:41 -0700

    Update CountNumberOfPairsWithAbsoluteDifferenceK.py

--- 2006. Count Number Of Pairs With Absolute Difference K/CountNumberOfPairsWithAbsoluteDifferenceK.py
+++ 2006. Count Number Of Pairs With Absolute Difference K/CountNumberOfPairsWithAbsoluteDifferenceK.py
@@ -1,14 +1,16 @@
-#It does not detect duplicates and the absolute value difference so i need to go back and fix this in my code
-Class Solution:
+class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
-        dict = {}
-        pairs = 0
+        dict = {} #dictionary
+        pairs = 0 #keep track of pairs
 
-        for num in nums:
-            dict[num] = len(nums)
+        for num in nums: #loop for creating dictionary
+            if num in dict:
+                dict[num] += 1 #increase frequency if exists in dictionary
+            else:
+                dict[num] = 1 #add to dictionary and initialize dictionary frequency to 1
 
         for num in dict:
-            if num - k in dict:
-                pairs = pairs + 1
+            if num - k in dict: #if the difference is found in dictionary
+                pairs += dict[num - k] * dict[num] #multiply the occurence by the frequency of it in the dictionary
 
-        return pairs
+        return pairs #return pairs
