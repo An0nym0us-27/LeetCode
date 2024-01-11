7172c562a599afbbcf285d7ae06a1820f78d0e09
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2024-01-10 21:54:23 -0800

    Create CountPairsOfSimilarStrings.py

---
+++ 2506. Count Pairs Of Similar Strings/CountPairsOfSimilarStrings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def similarPairs(self, words: List[str]) -> int:
+        
+        myDict= {} #create hashmap
+
+        for strings in words: #add strings to hashmap
+            myDict[strings] = len(strings)
+
+        for letters in myDict:
+            counter = 0
+            
+        return counter
