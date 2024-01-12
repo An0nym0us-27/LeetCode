65444d8e69bfd6dab9f05b6249256c5fdb8aa3eb
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2024-01-11 18:07:46 -0800

    Update CountPairsOfSimilarStrings.py

--- 2506. Count Pairs Of Similar Strings/CountPairsOfSimilarStrings.py
+++ 2506. Count Pairs Of Similar Strings/CountPairsOfSimilarStrings.py
@@ -1,12 +1,12 @@
+#This is NOT my code, used for learning purposes
+
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
-        
-        myDict= {} #create hashmap
+        counter = 0
 
-        for strings in words: #add strings to hashmap
-            myDict[strings] = len(strings)
-
-        for letters in myDict:
-            counter = 0
+        for strings in range(len(words)): #looped through strings
+            for letter in range(strings): #looped through letters
+                if set(words[strings]) == set(words[letter]): #if letters from the sets matched, increment counter (since sets must contain unique values)
+                    counter +=1
             
-        return counter
+        return counter #return result
