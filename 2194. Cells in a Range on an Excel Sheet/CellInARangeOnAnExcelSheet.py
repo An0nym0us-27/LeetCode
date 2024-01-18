a596edd76d5261c6487b0078987a6f8c3b0d3622
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2024-01-17 20:27:03 -0800

    Update CellInARangeOnAnExcelSheet.py

--- 2194. Cells in a Range on an Excel Sheet/CellInARangeOnAnExcelSheet.py
+++ 2194. Cells in a Range on an Excel Sheet/CellInARangeOnAnExcelSheet.py
@@ -1,18 +1,8 @@
+#This is not my code. Use to study
 class Solution:
     def cellsInRange(self, s: str) -> List[str]:
-        alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-        output = []
-        start = 0
-        end = 0
-
-        for location in s:
-            if location > start:
-                end = location
-            else:
-                for letter in alphabet:
-                    if location == letter:
-                        output.append(letter)
-                        while location != letter:
-                            output.append(letter)
-
-        return output
+        res = [] #result
+        for ch in range(ord(s[0]), ord(s[3])+1): #checks unicode letters inbetween start and end cell column letters
+            for i in range(int(s[1]), int(s[4])+1): #checks numbers in range of start and end row
+                res.append(f'{chr(ch)}{i}') #combines column and rows and appends them to list
+        return res#return result
