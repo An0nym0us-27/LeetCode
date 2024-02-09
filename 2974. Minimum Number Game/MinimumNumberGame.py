bccf7f6a5e2f3f00924e60884b34dbc8bbcfd309
Andrew C <98856300+An0nym0us-27@users.noreply.github.com>
2024-02-08 19:43:22 -0800

    Create MinimumNumberGame.py

---
+++ 2974. Minimum Number Game/MinimumNumberGame.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr = [] #output array
+        alice = 0 
+        bob = 0
+        while len(nums) > 0: #while list not empy
+            alice = min(nums) #store minimum int in alice
+            nums.remove(min(nums)) #remove min int
+            bob = min(nums) #store next min int
+            arr.append(bob)#append bob min int to output arr
+            nums.remove(min(nums)) #remove bob min int
+            arr.append(alice) #append alice min int to output arra
+        return arr #return output
